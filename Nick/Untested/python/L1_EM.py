4bc37a8a148b963b6c29980471aafb462735bc6e
NickPetta <74017854+NickPetta@users.noreply.github.com>
2020-11-09 14:05:20 -0600

    Create L1_EM.py
    init file

---
+++ Nick/Untested/python/L1_EM.py
@@ -0,0 +1,4 @@
+#this script toggles the state of the electro-magnet, with a delay to ensure the induced current
+#reaches zero beofre continuing the code, to ensure the bolt drops
+#this code toggles a gpio on GPIO connector 2
+
